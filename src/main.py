47bbfd636e7bebf1a46098dc3c7dcc567294bc8b
arailson <arailson.vieira@gmail.com>
2025-08-13 21:09:03 -0300

    teste de função de agendamento

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+import schedule
+import time
 from src.database import create_table, insert_data, consult_data
 from src.email import send_email
+from src.results import Results
 
 # results_create_table = create_table()
 # print("results_create_table:", results_create_table)
@@ -16,9 +19,42 @@ from src.email import send_email
 
 
 
-subject = "Teste de envio de e-mail"
-body = "Este é um e-mail de teste enviado automaticamente."
-to_email = "arailson.vieira@gmail.com"
+# subject = "Teste de envio de e-mail"
+# body = "Este é um e-mail de teste enviado automaticamente."
+# to_email = "arailson.vieira@gmail.com"
 
-result_email = send_email(subject, body, to_email)
-print(result_email)
+# result_email = send_email(subject, body, to_email)
+# print(result_email)
+
+def generate_weekly_report():
+    try:
+        return Results(success=True, data="Mensagem de teste relatorio semanal")
+    except Exception as e:
+        return Results(success=False, error_message=[str(e)])
+
+
+def send_weekly_email():
+    try:
+        subject = "Relatório Semanal"
+        body = generate_weekly_report().data
+        to_email = "arailson.vieira@gmail.com"
+        
+        result = send_email(subject, body, to_email)
+        
+        if result.success:
+            print(result.data)
+        else:
+            print(f"Error: {', '.join(result.error_message)}") 
+        
+        return Results(success=True, data=result.data)
+    except Exception as e:
+        return Results(success=False, error_message=[str(e)])
+
+
+# Agendar para todo sábado às 9:00 AM    
+schedule.every().thursday.at("09:00").do(send_weekly_email)
+
+# Loop para executar o agendamento
+while True:
+    schedule.run_pending()  # Executa as tarefas agendadas
+    time.sleep(60)
